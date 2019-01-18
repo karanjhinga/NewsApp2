7f6e6355d4fb0fb98055ec483fd2928402862901
karanjhinga <karanjhinga@gmail.com>
2019-01-18 14:29:13 +0530

    Added Icon and Splash Activity

---
+++ app/src/main/java/com/karanjhinga/newsapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.karanjhinga.newsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
